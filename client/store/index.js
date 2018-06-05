0e6f56bfc3c18c8f005587260b6aea18fda8461c
Patrick Ng <PJLN07@gmail.com>
2018-06-05 17:00:40 -0400

    added books reducer to store

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import books from './books'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user: user, books: books})
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
