663f3e8f553c407b251428414e981acdc1d4b64b
Wen Zhang <zhangwenfd@gmail.com>
2018-06-06 14:56:40 -0400

    add author reducer

---
+++ client/store/authors.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import history from '../history'
+
+// ACTION TYPES
+const GET_ALL_AUTHORS = 'GET_ALL_AUTHORS'
+
+// INITIAL STATE
+const allAuthors = [];
+
+//ACTION CREATORS
+const getAuthors = authors => ({
+  type: GET_ALL_Authors,
+  authors
+})
+
+//THUNK CREATORS
+export const gotAuthors = () => {
+  return async dispatch => {
+    const { data } = await axios.get('/api/authors')
+    dispatch(getAuthors(data))
+  }
+}
+
+// REDUCER
+export default function (state = allAuthors, action) {
+  switch (action.type) {
+    case GET_ALL_AUTHORS:
+      return action.authors
+    default:
+      return state
+  }
+}
