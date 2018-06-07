f5cfa0ec01d64d2cf40f33bbdf8590bfb78d9a94
Wen Zhang <zhangwenfd@gmail.com>
2018-06-07 16:58:55 -0400

    add review creator to seed script

--- client/components/index.js
+++ client/components/index.js
@@ -3,10 +3,10 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Navbar} from './navbar'
-export {default as UserHome} from './user-home'
-export {Login, Signup} from './auth-form'
-export {default as AllBooks} from './AllBooks'
-export {default as Sidebar} from './sidebar'
-export {default as AllAuthors} from './AllAuthors'
-export {default as ShoppingCart} from './ShoppingCart'
+export { default as Navbar } from './navbar'
+export { default as UserHome } from './user-home'
+export { Login, Signup } from './auth-form'
+export { default as AllBooks } from './AllBooks'
+export { default as Sidebar } from './Sidebar'
+export { default as AllAuthors } from './AllAuthors'
+export { default as ShoppingCart } from './ShoppingCart'
