bd5bacf8203e1500e341bd54e91893a321ff38ee
Matthew Contract <mcontract27@gmail.com>
2018-06-11 15:53:43 -0400

    implemented add book form

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,54 +1,62 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-import {logout} from '../store'
-import { UserHome } from '../components'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { logout } from "../store";
+import { UserHome } from "../components";
 
-const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div className='navbar'>
-    <h1 className="page-header">Barnes & Gable</h1>
-    <nav >
-      {isLoggedIn ? (
-        <div className="navbar-features">
-          <div className="navbar-links">
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
+const Navbar = ({ handleClick, isLoggedIn, user }) => {
+  console.log(user.admin);
+  return (
+    <div className="navbar">
+      <h1 className="page-header">Barnes & Gable</h1>
+      <nav>
+        {isLoggedIn ? (
+          <div className="navbar-features">
+            <div className="navbar-links">
+              <Link to="/home">Home</Link>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+              {user.admin ? <Link to="/admin">Admin</Link> : null}
+            </div>
+            <UserHome />
           </div>
-          <UserHome />
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        )}
+      </nav>
+      <hr />
+    </div>
+  );
+};
 
 /**
  * CONTAINER
  */
 const mapState = state => {
   return {
-    isLoggedIn: !!state.user.id
-  }
-}
+    isLoggedIn: !!state.user.id,
+    user: state.user
+  };
+};
 
 const mapDispatch = dispatch => {
   return {
     handleClick() {
-      dispatch(logout())
+      dispatch(logout());
     }
-  }
-}
+  };
+};
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(
+  mapState,
+  mapDispatch
+)(Navbar);
 
 /**
  * PROP TYPES
@@ -56,4 +64,4 @@ export default connect(mapState, mapDispatch)(Navbar)
 Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
-}
+};
--- client/routes.js
+++ client/routes.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { withRouter, Route, Switch } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import { Login, Signup, AllBooks, AllAuthors, AddReviewForm, ShoppingCart, PreviousPurchases } from './components'
-import { me } from './store'
-import { gotBooks } from './store/books'
-import { gotAuthors } from './store/authors'
-import SingleBook from './components/SingleBook'
-import SingleAuthor from './components/SingleAuthor'
-import AllGenres from './components/AllGenres'
-import BooksByGenres from './components/BooksByGenres';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch } from "react-router-dom";
+import PropTypes from "prop-types";
+import {
+  Login,
+  Signup,
+  AllBooks,
+  AllAuthors,
+  AddReviewForm,
+  ShoppingCart,
+  PreviousPurchases,
+  Admin
+} from "./components";
+import { me } from "./store";
+import { gotBooks } from "./store/books";
+import { gotAuthors } from "./store/authors";
+import SingleBook from "./components/SingleBook";
+import SingleAuthor from "./components/SingleAuthor";
+import AllGenres from "./components/AllGenres";
+import BooksByGenres from "./components/BooksByGenres";
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
-
-
   componentDidMount() {
-    this.props.loadInitialData()
-    this.props.gotBooks()
-    this.props.gotAuthors()
+    this.props.loadInitialData();
+    this.props.gotBooks();
+    this.props.gotAuthors();
   }
 
   render() {
@@ -29,50 +36,56 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-            {/* Routes placed here are only available after logging in */}
-            <Route exact path="/genres/:genre" component={BooksByGenres} />
-            <Route exact path="/genres" component={AllGenres} />
-            <Route exact path="/books/:bookId" component={SingleBook} />
-            <Route exact path="/new-review/:bookId" component={AddReviewForm} />
-            <Route exact path="/home" component={AllBooks} />
-            <Route exact path="/authors/:authorId" component={SingleAuthor} />
-            <Route exact path="/cart" component={ShoppingCart} />
-            <Route exact path="/history" component={PreviousPurchases} />
-            {/* <Route exact path="/authors/:authorId" component={BooksByGenres} /> */}
-            <Route exact path="/authors" component={AllAuthors} />
-            <Route exact path="/" component={AllBooks} />
+        {/* Routes placed here are only available after logging in */}
+        <Route exact path="/genres/:genre" component={BooksByGenres} />
+        <Route exact path="/genres" component={AllGenres} />
+        <Route exact path="/books/:bookId" component={SingleBook} />
+        <Route exact path="/new-review/:bookId" component={AddReviewForm} />
+        <Route exact path="/home" component={AllBooks} />
+        <Route exact path="/authors/:authorId" component={SingleAuthor} />
+        <Route exact path="/cart" component={ShoppingCart} />
+        <Route exact path="/history" component={PreviousPurchases} />
+        <Route exact path="/admin" component={Admin} />
+        {/* <Route exact path="/authors/:authorId" component={BooksByGenres} /> */}
+        <Route exact path="/authors" component={AllAuthors} />
+        <Route exact path="/" component={AllBooks} />
         {/* Displays our Login component as a fallback */}
       </Switch>
-    )
+    );
   }
 }
 
 /**
  * CONTAINER
  */
-const mapState = (state) => {
+const mapState = state => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
     isLoggedIn: !!state.user.id,
     books: state.books,
     authors: state.authors
-  }
-}
+  };
+};
 
-const mapDispatch = (dispatch) => {
+const mapDispatch = dispatch => {
   return {
     loadInitialData() {
-      dispatch(me())
+      dispatch(me());
     },
     gotBooks: () => dispatch(gotBooks()),
     gotAuthors: () => dispatch(gotAuthors())
-  }
-}
+  };
+};
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
+export default withRouter(
+  connect(
+    mapState,
+    mapDispatch
+  )(Routes)
+);
 
 /**
  * PROP TYPES
@@ -80,4 +93,4 @@ export default withRouter(connect(mapState, mapDispatch)(Routes))
 Routes.propTypes = {
   loadInitialData: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
-}
+};
