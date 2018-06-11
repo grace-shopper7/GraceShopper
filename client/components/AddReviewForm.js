10abb180a4362eb6ff14b78951ef5766609d8e80
Patrick Ng <PJLN07@gmail.com>
2018-06-11 10:35:55 -0400

    added some divs for styling

--- client/components/AddReviewForm.js
+++ client/components/AddReviewForm.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { addedReview } from '../store/reviews'
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { addedReview } from "../store/reviews";
 
 class AddReviewForm extends React.Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit (event) {
+  handleSubmit(event) {
     event.preventDefault();
     let rating = event.target.rating.value;
     let text = event.target.text.value;
@@ -20,33 +20,36 @@ class AddReviewForm extends React.Component {
       text,
       bookId,
       userId
-    }
-    this.props.addReview(formData, bookId)
-    this.props.history.push(`/books/${bookId}`)
+    };
+    this.props.addReview(formData, bookId);
+    this.props.history.push(`/books/${bookId}`);
   }
 
-  render () {
-    if (this.props.books.length === 0){
-      return <div>Loading...</div>
+  render() {
+    if (this.props.books.length === 0) {
+      return <div>Loading...</div>;
     }
 
-    let currentBook = this.props.books.filter((book) => {
-      return (book.id === +this.props.match.params.bookId)
-    })
+    let currentBook = this.props.books.filter(book => {
+      return book.id === +this.props.match.params.bookId;
+    });
 
     return (
       <div id="review-form-container">
-      <div id="review-book-info">
-        <h2>Tell us about</h2>
-        <div id="review-book-cover">
-          <img src={currentBook[0].imageUrl} />
+        <div id="review-book-info">
+          <h2>Tell us about</h2>
+          <div id="review-book-cover">
+            <img src={currentBook[0].imageUrl} />
+          </div>
+          <h3>
+            {`${currentBook[0].title} by ${currentBook[0].author.firstName} ${
+              currentBook[0].author.lastName
+            }`}
+          </h3>
         </div>
-        <h3>{`${currentBook[0].title} by ${currentBook[0].author.firstName} ${currentBook[0].author.lastName}`}
-        </h3>
-      </div>
-      <div id="review-form-itself">
-        <form id="review-form" onSubmit={this.handleSubmit}>
-          <label htmlFor="rating"> Rating </label>
+        <div id="review-form-itself">
+          <form id="review-form" onSubmit={this.handleSubmit}>
+            <label htmlFor="rating"> Rating </label>
             <select name="rating">
               <option value="1">1</option>
               <option value="2">2</option>
@@ -54,22 +57,22 @@ class AddReviewForm extends React.Component {
               <option value="1">4</option>
               <option value="1">5</option>
             </select>
-          <label htmlFor="text"> Description </label>
+            <label htmlFor="text"> Description </label>
             <textarea name="text" placeholder="Write your review here" />
-          <div id="review-form-buttons">
-            <div id="review-cancel-button" >
-              <Link to={`/books/${this.props.match.params.bookId}`} >
-              <button type="button" > Cancel </button>
-              </Link>
+            <div id="review-form-buttons">
+              <div id="review-cancel-button">
+                <Link to={`/books/${this.props.match.params.bookId}`}>
+                  <button type="button"> Cancel </button>
+                </Link>
+              </div>
+              <div id="review-submit-button">
+                <button type="submit">Submit</button>
+              </div>
             </div>
-            <div id="review-submit-button">
-              <button type="submit" >Submit</button>
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-      </div>
-    )
+    );
   }
 }
 
@@ -77,13 +80,16 @@ const mapState = state => {
   return {
     books: state.books,
     user: state.user
-  }
-}
+  };
+};
 
 const mapDispatch = dispatch => {
   return {
     addReview: (formData, bookId) => dispatch(addedReview(formData, bookId))
-  }
-}
+  };
+};
 
-export default connect(mapState, mapDispatch)(AddReviewForm)
+export default connect(
+  mapState,
+  mapDispatch
+)(AddReviewForm);
