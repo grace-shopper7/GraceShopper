990001b1649344a9f8e44ad5f1179e1e8cf7aedd
Patrick Ng <PJLN07@gmail.com>
2018-06-12 12:16:44 -0400

    CHANGE added tests to test if Links are rendered and how many

--- client/components/AllGenres.spec.js
+++ client/components/AllGenres.spec.js
@@ -1,24 +1,53 @@
 /* global describe beforeEach it */
 
-import {expect} from 'chai'
-import React from 'react'
-import enzyme, {shallow} from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-import {AllGenres} from './AllGenres'
+import { expect } from "chai";
+import React from "react";
+import { Link } from "react-router-dom";
+import enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { AllGenres } from "./AllGenres";
 
-const adapter = new Adapter()
-enzyme.configure({adapter})
+const adapter = new Adapter();
+enzyme.configure({ adapter });
 
-describe('AllGenres', () => {
+describe("AllGenres", () => {
   let wrapper;
 
-  beforeEach(() => {
-    let resultArr = [];
-    let books = [{genre: 'Horror'}];
-    wrapper = shallow(<AllGenres books={books} />)
-  })
+  describe("renders a Link", () => {
+    beforeEach(() => {
+      let resultArr = [];
+      let books = [{ genre: "Horror" }];
+      wrapper = shallow(<AllGenres books={books} />);
+    });
 
-    it('renders Link with genre name', () => {
-    expect(wrapper.find('Link').text()).to.equal('Horror')
-  })
-})
+    it("renders one Link with only one book", () => {
+      expect(wrapper.find("Link").length).to.equal(1);
+    });
+
+    it("renders a div with Link inside it", () => {
+      expect(
+        wrapper.contains(
+          <div className="each-genre" key="Horror">
+            <Link to={`/genres/Horror`}>{"Horror"}</Link>
+          </div>
+        )
+      ).to.equal(true);
+    });
+  });
+
+  describe("renders multiple Links for multiple books", () => {
+    beforeEach(() => {
+      let resultArr = [];
+      let books = [
+        { genre: "Horror" },
+        { genre: "Fantasy" },
+        { genre: "Romance" }
+      ];
+      wrapper = shallow(<AllGenres books={books} />);
+    });
+
+    it("renders more than one Link with multiple books", () => {
+      expect(wrapper.find("Link").length).to.equal(3);
+    });
+  });
+});
